39d7806b454171a051ac6a73dbb636d76f5601b8
trevinofernando <trevino.96@hotmail.com>
2020-03-22 14:48:51 -0400

    Added final strategy

---
+++ src/StrategyFernandoYaclaudes.java
@@ -0,0 +1,41 @@
+
+/**
+ * Class containing the strategy developed by the Genetic Algorithm.
+ * 
+ * @author Fernando Trevino Ramirez
+ */
+import java.util.*;
+
+public class StrategyFernandoYaclaudes extends Strategy {
+    /**
+     * Encoding for a strategy.
+     */
+    double ProbabilityOfCooperatingGivenCooperation = 0.95;
+    double ProbabilityOfCooperatingGivenDefection = 0.15;
+
+    // 0 = defect, 1 = cooperate
+
+    public StrategyFernandoYaclaudes() {
+        name = "Strategy Developed by Fernando and Yaclaudes";
+        opponentLastMove = 1;
+    } /* StrategyFernandoYaclaudes */
+
+    public int nextMove() {
+        // Default p to Probability Of Cooperating Given Cooperation
+        double p = ProbabilityOfCooperatingGivenCooperation;
+
+        // If opponentLastMove == defect
+        if (opponentLastMove == 0) {
+            // Change p to be Probability Of Cooperating Given Defection
+            p = ProbabilityOfCooperatingGivenDefection;
+        }
+
+        // Decision given on random chance
+        if (p > Search.r.nextDouble()) {
+            return 1;
+        } else {
+            return 0;
+        }
+
+    } /* nextMove */
+} /* class StrategyFernandoYaclaudes */
